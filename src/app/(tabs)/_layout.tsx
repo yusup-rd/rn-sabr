7f1de2bc2463a03f1c7a00e43d3bd20bab4208a0
Yusup <yusuprd@icloud.com>
2026-09-14 22:50:31 +0500

    feat: setup native tabs for IOS

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,77 +1,121 @@
 import { theme } from "@/constants/theme";
 import { FontAwesome6 as Fa } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
+import { NativeTabs } from "expo-router/unstable-native-tabs";
+import { Platform } from "react-native";
 
 const TabsLayout = () => {
-  return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarActiveTintColor: theme.colors.primary,
-        tabBarInactiveTintColor: theme.colors.mutedForeground,
+  if (Platform.OS === "ios") {
+    return (
+      <NativeTabs tintColor={theme.colors.primary}>
+        <NativeTabs.Trigger name="index">
+          <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
+          <NativeTabs.Trigger.Icon
+            sf={{ default: "house", selected: "house.fill" }}
+            md="home"
+          />
+        </NativeTabs.Trigger>
+        <NativeTabs.Trigger name="quran">
+          <NativeTabs.Trigger.Label>Quran</NativeTabs.Trigger.Label>
+          <NativeTabs.Trigger.Icon
+            sf={{ default: "book.closed", selected: "book.closed.fill" }}
+            md="menu_book"
+          />
+        </NativeTabs.Trigger>
+        <NativeTabs.Trigger name="qibla">
+          <NativeTabs.Trigger.Label>Qibla</NativeTabs.Trigger.Label>
+          <NativeTabs.Trigger.Icon
+            sf={{ default: "location.north", selected: "location.north.fill" }}
+            md="explore"
+          />
+        </NativeTabs.Trigger>
+        <NativeTabs.Trigger name="mosques">
+          <NativeTabs.Trigger.Label>Mosques</NativeTabs.Trigger.Label>
+          <NativeTabs.Trigger.Icon
+            sf={{ default: "mappin", selected: "mappin.and.ellipse" }}
+            md="location_on"
+          />
+        </NativeTabs.Trigger>
+        <NativeTabs.Trigger name="settings">
+          <NativeTabs.Trigger.Label>Settings</NativeTabs.Trigger.Label>
+          <NativeTabs.Trigger.Icon
+            sf={{ default: "gearshape", selected: "gearshape.fill" }}
+            md="settings"
+          />
+        </NativeTabs.Trigger>
+      </NativeTabs>
+    );
+  } else {
+    return (
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarActiveTintColor: theme.colors.primary,
+          tabBarInactiveTintColor: theme.colors.mutedForeground,
 
-        tabBarStyle: {
-          backgroundColor: theme.colors.card,
-          borderTopColor: theme.colors.border,
-        },
+          tabBarStyle: {
+            backgroundColor: theme.colors.card,
+            borderTopColor: theme.colors.border,
+          },
 
-        tabBarLabelStyle: {
-          fontFamily: "sans-medium",
-          fontSize: 11,
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color, size }) => (
-            <Fa name="mosque" size={size} color={color} />
-          ),
+          tabBarLabelStyle: {
+            fontFamily: "sans-medium",
+            fontSize: 11,
+          },
         }}
-      />
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color, size }) => (
+              <Fa name="mosque" size={size} color={color} />
+            ),
+          }}
+        />
 
-      <Tabs.Screen
-        name="quran"
-        options={{
-          title: "Quran",
-          tabBarIcon: ({ color, size }) => (
-            <Fa name="book-quran" size={size} color={color} />
-          ),
-        }}
-      />
+        <Tabs.Screen
+          name="quran"
+          options={{
+            title: "Quran",
+            tabBarIcon: ({ color, size }) => (
+              <Fa name="book-quran" size={size} color={color} />
+            ),
+          }}
+        />
 
-      <Tabs.Screen
-        name="qibla"
-        options={{
-          title: "Qibla",
-          tabBarIcon: ({ color, size }) => (
-            <Fa name="compass" size={size} color={color} />
-          ),
-        }}
-      />
+        <Tabs.Screen
+          name="qibla"
+          options={{
+            title: "Qibla",
+            tabBarIcon: ({ color, size }) => (
+              <Fa name="compass" size={size} color={color} />
+            ),
+          }}
+        />
 
-      <Tabs.Screen
-        name="mosques"
-        options={{
-          title: "Mosques",
-          tabBarIcon: ({ color, size }) => (
-            <Fa name="location-dot" size={size} color={color} />
-          ),
-        }}
-      />
+        <Tabs.Screen
+          name="mosques"
+          options={{
+            title: "Mosques",
+            tabBarIcon: ({ color, size }) => (
+              <Fa name="location-dot" size={size} color={color} />
+            ),
+          }}
+        />
 
-      <Tabs.Screen
-        name="settings"
-        options={{
-          title: "Settings",
-          tabBarIcon: ({ color, size }) => (
-            <Fa name="gear" size={size} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
-  );
+        <Tabs.Screen
+          name="settings"
+          options={{
+            title: "Settings",
+            tabBarIcon: ({ color, size }) => (
+              <Fa name="gear" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    );
+  }
 };
 
 export default TabsLayout;
