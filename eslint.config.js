6dd7d6c2621dfab56327779418d649c4d1341cad
Yusup <yusuprd@icloud.com>
2026-09-23 00:45:34 +0500

    chore: add lint check for non localized jsx

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,24 @@
-// https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
+const i18next = require("eslint-plugin-i18next");
 
 module.exports = defineConfig([
   expoConfig,
   {
     ignores: ["dist/*"],
+  },
+  {
+    plugins: {
+      i18next,
+    },
+    rules: {
+      "i18next/no-literal-string": [
+        "warn",
+        {
+          mode: "jsx-only",
+        },
+      ],
+    },
   },
   {
     files: ["src/i18n/index.ts"],
